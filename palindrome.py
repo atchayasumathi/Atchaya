9c2d0d13329d9395e614ebabd57215c7687eafc1
atchayasumathi <50608682+atchayasumathi@users.noreply.github.com>
2019-05-15 13:57:51 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,11 @@
 n=int(input("enter a number"))
 sum=0
 temp=n
-while(n>0):
-    rem=n%10
-    sum=sum*10+rem
-    n=n//10
+if(n<=1000):
+    while(n>0):
+        rem=n%10
+        sum=sum*10+rem
+        n=n//10
 if(sum==temp):
     print("yes palindrome")
 else:
